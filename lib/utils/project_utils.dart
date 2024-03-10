24d6fcfc22a6374c86114f09622d917c0a1ae676
Riyan Khan <riyankhanpyrex01@gmail.com>
2024-03-10 19:34:55 +0400

    [init]

---
+++ lib/utils/project_utils.dart
@@ -0,0 +1,30 @@
+class ProjectUtils {
+  static const List<String> banners = [
+    "assets/projects/Kryptoria.jfif",
+    "assets/projects/siki.png",
+    "assets/projects/nav_companion.png",
+    "assets/projects/e-commerce.jfif",
+  ];
+
+  static const List<String> icons = [
+    "assets/projects/kryptoria_icon.jpg",
+    "assets/projects/siki_icon.png",
+    "assets/projects/nav_companion_icon.png",
+    "assets/projects/ecommerce_logo.png",
+  ];
+
+  static const List<String> titles = [
+    "Web3 based game",
+    "Web3 based song App",
+    "Co-navigation map app",
+    "E commerce cross platform app",
+  ];
+
+  static const List<String> description = [
+    "Its a web3 based card game where your NFTs act like in game items such as charachter and its gun",
+    "Its a web3 based song app where a song is NFT minted by the artist",
+    "Its a google map like app where people can travel togather in group like manner where each of them are available on map",
+    "Its a traditional e commerce app where items are listed by admin/store owner and can be selled",
+    ];
+
+}
