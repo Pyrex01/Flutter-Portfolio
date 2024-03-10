b5228db14d1809110aee6b2ea47e8bcda674726f
Riyan Khan <riyankhanpyrex01@gmail.com>
2024-03-10 19:34:55 +0400

    [init]

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Riyan Khan, a Full Stack Developer and Flutter Developer.';
+
+  static const String aboutMeDetail =
+      "A software engineer, I'm driven by an insatiable curiosity for solving complex problems with precision. My expertise spans from bare-metal computer architecture to creating cross-platform apps. Proficient in programming processors, networking, and web development, I bring a versatile skill set to the table as a dedicated full-stack developer";
+}
---
+++ lib/utils/services_utils.dart
@@ -0,0 +1,24 @@
+import 'package:folio/utils/utils.dart';
+
+class ServicesUtils {
+  static const List<String> servicesIcons = [
+    StaticUtils.appDev,
+    StaticUtils.rapid,
+    StaticUtils.blog,
+    StaticUtils.openSource,
+  ];
+
+  static const List<String> servicesTitles = [
+    "Mobile App Development",
+    "Backend Development",
+    "Microservices Development",
+    "Open Source - GitHub",
+  ];
+
+  static const List<String> servicesDescription = [
+    "Android app development via Flutter\n- Splash Screen\n- Firebase Auth/Cloud\n- REST APIs\n- Maps integration and more...!",
+    "Backend development via multiple technologies : NestJs, SprintBoot, ActixWeb, Django, and more",
+    "Microservices development with multiple technologies inter communication ...!",
+    "Open source GitHub Projects\n- Awesome README.md\n- Well documented\n- Header images and more...!",
+  ];
+}
